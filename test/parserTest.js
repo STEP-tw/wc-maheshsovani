2474e99ef7666710d8b1ca8782165a9700b2807b
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2018-12-27 09:23:05 +0530

    Added test cases for parser function and modified it statements.

--- test/parserTest.js
+++ test/parserTest.js
@@ -8,9 +8,33 @@ describe("parser", function() {
     assert.deepEqual(actualOutput, expectedOutput);
   });
 
-  it("should return option and files when valid option is given", function() {
+  it("should return option and files when only one option is given", function() {
     actualOutput = parser(["-l", "numbers"]);
     expectedOutput = { options: "l", files: ["numbers"] };
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+
+  it("should return option and files when two options are given seperately", function() {
+    actualOutput = parser(["-l", "-w", "numbers"]);
+    expectedOutput = { options: "lw", files: ["numbers"] };
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+
+  it("should return option and files when two options are given combined", function() {
+    actualOutput = parser(["-lw", "numbers"]);
+    expectedOutput = { options: "lw", files: ["numbers"] };
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+
+  it("should return option and files when three options are given seperately", function() {
+    actualOutput = parser(["-l", "-w", "-c", "numbers"]);
+    expectedOutput = { options: "lwc", files: ["numbers"] };
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+
+  it("should return option and files when three options are given combined", function() {
+    actualOutput = parser(["-lwc", "numbers"]);
+    expectedOutput = { options: "lwc", files: ["numbers"] };
     assert.deepEqual(actualOutput, expectedOutput);
   });
 });
